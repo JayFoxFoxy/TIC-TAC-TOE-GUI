caa81eee96e6ec76b1ad0bebc252401a86960876
jayfoxfoxy <joao.p.s.pinho@gmail.com>
2019-11-13 22:17:44 +0000

    lessss goooooo

--- src/menu2.java
+++ src/menu2.java
@@ -1,16 +1,42 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class menu2 {
 
-	public static void buildingthepanel(Container frame){
+	static int move = 0;
+	
+	public static void buildingthepanel(Container frame, int board[][]){
 			 
 			JLabel lb1; // Alias to create labels
-			JButton bt1, bt2, bt3, bt4, bt5, bt6; // Alias to create the buttons
+			JButton bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9; // Alias to create the buttons
 			JMenuBar m1;
 			JMenu mn1, mn2; // Alias to create a menu objects
 			JMenuItem menuItem; // Alias to create the index on the menu objects
 			GridBagConstraints gbc; // Alias to create the constraints
+			
+			int p1 = gotValue(board, 0, 0);
+			int p2 = gotValue(board, 0, 1);
+			int p3 = gotValue(board, 0, 2);
+			int p4 = gotValue(board, 1, 0);
+			int p5 = gotValue(board, 1, 1);
+			int p6 = gotValue(board, 1, 2);
+			int p7 = gotValue(board, 2, 0);
+			int p8 = gotValue(board, 2, 1);
+			int p9 = gotValue(board, 2, 2);
+			
+			String pos00 = changeVariableValue(p1);
+			String pos01 = changeVariableValue(p2);
+			String pos02 = changeVariableValue(p3);
+			String pos10 = changeVariableValue(p4);
+			String pos11 = changeVariableValue(p5);
+			String pos12 = changeVariableValue(p6);
+			String pos30 = changeVariableValue(p7);
+			String pos31 = changeVariableValue(p8);
+			String pos32 = changeVariableValue(p9);
+			
+			
 			
 			
 			
@@ -40,53 +66,186 @@ public class menu2 {
 			gbc.fill = GridBagConstraints.HORIZONTAL;
 			gbc.gridy = 0;
 		    gbc.gridx = 0;
-		    //gbc.gridwidth = 2;
+		    gbc.gridwidth = 3;
 		    frame.add(m1, gbc);
 			
 			
 			
 			
-			bt1 = new JButton("Button 1");
+			bt1 = new JButton(String.valueOf(pos00));
+			bt1.addActionListener(new ActionListener() {
+				
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					int player = whoPlay(move);
+					board[0][0] = player; 
+					int value = gotValue(board, 0, 0);
+					String nw = changeVariableValue(value);
+					bt1.setText(nw);
+					bt1.setEnabled(false);
+					move = move + 1;
+				}
+			});
+			
+			gbc.gridwidth = 1;
 			//gbc.weightx = 1.0;
 		    gbc.fill = GridBagConstraints.HORIZONTAL;
 		    gbc.gridy = 1;
-		    gbc.gridx = 0;
+		    gbc.gridx = 0; 
 		    frame.add(bt1, gbc);
 		    
-		    bt2 = new JButton("Button 2");
+		    bt2 = new JButton(String.valueOf(pos01));
+		    bt2.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					int player = whoPlay(move);
+					board[0][1] = player; 
+					int value = gotValue(board, 0, 1);
+					String nw = changeVariableValue(value);
+					bt2.setText(nw);
+					bt2.setEnabled(false);
+					move = move + 1;
+				}
+			});
 			//gbc.weightx = 0.0;
 		    gbc.fill = GridBagConstraints.HORIZONTAL;
 		    gbc.gridy = 1;
 		    gbc.gridx = 1;
 		    frame.add(bt2, gbc);
 		    
-		    bt3 = new JButton("Button 3");
+		    bt3 = new JButton(String.valueOf(pos02));
+		    bt3.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					int player = whoPlay(move);
+					board[0][2] = player; 
+					int value = gotValue(board, 0, 2);
+					String nw = changeVariableValue(value);
+					bt3.setText(nw);
+					bt3.setEnabled(false);
+					move = move + 1;
+				}
+			});
 			//gbc.weightx = 0.0;
 		    gbc.fill = GridBagConstraints.HORIZONTAL;
-		    gbc.gridy = 2;
-		    gbc.gridx = 0;
+		    gbc.gridy = 1;
+		    gbc.gridx = 2;
 		    frame.add(bt3, gbc);
 		    
-		    bt4 = new JButton("Button 4");
+		    bt4 = new JButton(String.valueOf(pos10));
+		    bt4.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					int player = whoPlay(move);
+					board[1][0] = player; 
+					int value = gotValue(board, 1, 0);
+					String nw = changeVariableValue(value);
+					bt4.setText(nw);
+					bt4.setEnabled(false);
+					move = move + 1;
+				}
+			});
+			//gbc.weightx = 0.0;
+		    gbc.fill = GridBagConstraints.HORIZONTAL;
+		    gbc.gridy = 2;
+		    gbc.gridx = 0;
+		    frame.add(bt4, gbc);
+		    
+		    bt5 = new JButton(String.valueOf(pos11));
+		    bt5.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					int player = whoPlay(move);
+					board[1][1] = player; 
+					int value = gotValue(board, 1, 1);
+					String nw = changeVariableValue(value);
+					bt5.setText(nw);
+					bt5.setEnabled(false);
+					move = move + 1;
+				}
+			});
 			//gbc.weightx = 0.0;
 		    gbc.fill = GridBagConstraints.HORIZONTAL;
 		    gbc.gridy = 2;
 		    gbc.gridx = 1;
-		    frame.add(bt4, gbc);
+		    frame.add(bt5, gbc);
 		    
-		    bt5 = new JButton("Button 5");
+		    bt6 = new JButton(String.valueOf(pos12));
+		    bt6.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					int player = whoPlay(move);
+					board[1][2] = player; 
+					int value = gotValue(board, 1, 2);
+					String nw = changeVariableValue(value);
+					bt6.setText(nw);
+					bt6.setEnabled(false);
+					move = move + 1;
+				}
+			});
+			//gbc.weightx = 0.0;
+		    gbc.fill = GridBagConstraints.HORIZONTAL;
+		    gbc.gridy = 2;
+		    gbc.gridx = 2;
+		    frame.add(bt6, gbc);
+		    
+		    bt7 = new JButton(String.valueOf(pos30));
+		    bt7.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					int player = whoPlay(move);
+					board[2][0] = player; 
+					int value = gotValue(board, 2, 0);
+					String nw = changeVariableValue(value);
+					bt7.setText(nw);
+					bt7.setEnabled(false);
+					move = move + 1;
+				}
+			});
 			//gbc.weightx = 0.0;
 		    gbc.fill = GridBagConstraints.HORIZONTAL;
 		    gbc.gridy = 3;
 		    gbc.gridx = 0;
-		    frame.add(bt5, gbc);
+		    frame.add(bt7, gbc);
 		    
-		    bt6 = new JButton("Button 6");
+		    bt8 = new JButton(String.valueOf(pos31));
+		    bt8.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					int player = whoPlay(move);
+					board[2][1] = player; 
+					int value = gotValue(board, 2, 1);
+					String nw = changeVariableValue(value);
+					bt8.setText(nw);
+					bt8.setEnabled(false);
+					move = move + 1;
+				}
+			});
 			//gbc.weightx = 0.0;
 		    gbc.fill = GridBagConstraints.HORIZONTAL;
 		    gbc.gridy = 3;
 		    gbc.gridx = 1;
-		    frame.add(bt6, gbc);
+		    frame.add(bt8, gbc);
+		    
+		    bt9 = new JButton(String.valueOf(pos32));
+		    bt9.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					int player = whoPlay(move);
+					board[2][2] = player; 
+					int value = gotValue(board, 2, 2);
+					String nw = changeVariableValue(value);
+					bt9.setText(nw);
+					bt9.setEnabled(false);
+					move = move + 1;
+				}
+			});
+			//gbc.weightx = 0.0;
+		    gbc.fill = GridBagConstraints.HORIZONTAL;
+		    gbc.gridy = 3;
+		    gbc.gridx = 2;
+		    frame.add(bt9, gbc);
 		    
 		    lb1 = new JLabel("TIC TAC TOE");
 		    //gbc.ipady = 200;
@@ -94,7 +253,7 @@ public class menu2 {
 		    gbc.fill = GridBagConstraints.HORIZONTAL;
 		    gbc.gridy = 4;
 		    gbc.gridx = 0;
-		    gbc.gridwidth = 2;
+		    gbc.gridwidth = 3;
 		    frame.add(lb1, gbc);
 		    
 			
@@ -102,7 +261,7 @@ public class menu2 {
 	
 	public static void showGui() {
 		
-		//int[][] board = new int[3][3];
+		int[][] board = new int[3][3];
 		
 		JFrame frame = new JFrame("TIC TAC TOE");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -110,7 +269,7 @@ public class menu2 {
         //JPanel panel = new JPanel();
  
         //Set up the content pane.
-        buildingthepanel(frame.getContentPane());
+        buildingthepanel(frame.getContentPane(), board);
  
         //Display the window.
         //frame.setSize(400, 400);
@@ -129,6 +288,48 @@ public class menu2 {
                 showGui();
             }
         });
+	}
+	
+	static String changeVariableValue(int value) {
+		
+		String c;
+		
+		if(value == 0) {
+			c = "*";
+		}else if(value == 1) {
+			c = "X";
+		}else {
+			c = "O";
+		}
+		
+		
+		return c;
+		
+	}
+	
+	static int gotValue(int board[][], int r, int c) {
+		
+		int value;
+		
+		value = board[r][c];
+		
+		
+		return value;
+		
+	}
+	
+	static int whoPlay(int value) {
+		
+		int r;
+		
+		if(value % 2 == 0) {
+			r = 1;
+		}else {
+			r = 2;
+		}
+		
+		return r;
+		
 	}
 
 }
